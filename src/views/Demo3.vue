8cada2688bec0790867d82c35462675bb8519e98
tomimandalap <tommymandalaputra@gmail.com>
2023-12-14 18:46:19 +0700

    adding styling component demo3

--- src/views/Demo3.vue
+++ src/views/Demo3.vue
@@ -94,3 +94,31 @@ function handleOnCanStop() {
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.phone {
+  @apply relative h-full w-[375px] border-4 border-[#111] bg-gray-300/20 rounded-3xl;
+
+  & .container-notch {
+    @apply absolute top-0 left-0 right-0 rounded-t-2xl;
+
+    & .notch {
+      @apply left-[50%] md:w-[10vw] w-[40vw] h-[4.25vh] my-0 mx-auto bg-[#111] rounded-b-xl;
+    }
+  }
+
+  & .content {
+    @apply flex flex-col justify-center items-center min-h-screen rounded-3xl;
+
+    & .container-btn-stop {
+      @apply absolute top-[calc(100%-10%)] left-[50%] m-auto;
+      -webkit-transform: translate(-50%, -50%);
+      transform: translate(-50%, -50%);
+    }
+
+    & .container-btn-facemode {
+      @apply absolute top-[calc(100%-12%)] left-[calc(100%-85%)] m-auto;
+    }
+  }
+}
+</style>
